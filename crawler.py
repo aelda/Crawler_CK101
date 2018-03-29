4bc7a9f82937103deb30d73b738c155952f94863
kate <kate1011good@gmail.com>
2018-03-29 14:33:41 +0800

    Add files via upload
    alternation

--- crawler.py
+++ crawler.py
@@ -18,21 +18,10 @@ def sslwrap(func):
 
 ssl.wrap_socket = sslwrap(ssl.wrap_socket)
 
-'''
-url = 'https://ithome.com.tw/'
-resp = requests.get(url)
-soup = BeautifulSoup(resp.text,'html.parser')
-dcard_title = soup.find_all('p',class_ = {'title'})
-f = open('result.txt','w')
-f.write('iThome\n')
-for index , title in enumerate(dcard_title[:10]):
-    f.write("{}. {} \n".format(index + 1, title.text))
-f.close()
-'''
-# 403 Http problem 
-headers = ('Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1')
+url = input ("input the noval url at CK101 website: ")
 
-url = input ("input the noval website url : ")
+if not os.path.exists('./noval') :
+    os.mkdir('noval')
 
 driver = webdriver.Chrome() # chrome driver
 driver.get(url) 
@@ -40,22 +29,13 @@ driver.get(url)
 url = url[:-8]
 filename = ''
 page = 1;
-total_page = 17
+requestUrl(url,page)
+total_page = soup.find(class_='last').text
+
 while page < total_page + 1:
-    # everytime open the url , must use urllib to request the website 
-    req = urllib.request.Request(url + str(page)+'-1.html')  # for 403 HTTP Forbidden, some website forbid crawler
-    req.add_header("User-Agent",headers)
-    files =urllib.request.urlopen(req)
-
-    soup = BeautifulSoup(files , 'html5lib')
-    # create the dir
-    if page == 1 :
-        dir_name = 'noval'
-        if not os.path.exists('./' + dir_name) :
-            os.mkdir(dir_name)
-
+    requestUrl(url,page)
     filename = 'Page' + str(page)
-    f = open('./'+ dir_name +'/' + filename + '.txt','w')
+    f = open('./noval/' + filename + '.txt','w')
     f.write(soup.title.string)
     lines = soup.select('.postList #pbody .mes .postmessage') # array of content
     index = 0
@@ -63,16 +43,23 @@ while page < total_page + 1:
     for line in lines :
         f.write(line.text)
         index = index + 1
+        # progressbar
         sys.stdout.write('\r')
-        sys.stdout.write('[%-50s] %d%%' % ('='*int(50/lens*index),index/lens*100))  # progressbar 
+        sys.stdout.write('[%-50s] %d%%' % ('='*int(50/lens*index),index/lens*100))   
         sys.stdout.flush()
-    driver.find_element_by_xpath("//a[contains(text(),'下一頁')]").click()  # click to next page
+    # click to next page    
+    driver.find_element_by_xpath("//a[contains(text(),'下一頁')]").click()  
     page = page + 1
     sys.stdout.write('\n'+ soup.title.string+ ' saved \n')
     f.close()
     time.sleep(2)
 driver.quit()
 
-
-
+def requestUrl(url,page):
+    # everytime open the url , must use urllib to request the website 
+    headers = ('Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1')
+    req = urllib.request.Request(url + str(page)+'-1.html')  # for 403 HTTP Forbidden, some website forbid crawler
+    req.add_header("User-Agent",headers)
+    files = urllib.request.urlopen(req)
+    soup = BeautifulSoup(files , 'html5lib')
 
